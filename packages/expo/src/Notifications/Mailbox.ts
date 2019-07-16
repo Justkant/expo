1553436fa41170812373ebacd30e8220b00d2aa1
szymon <szymon.kapala@swmansion.com>
2019-07-16 09:31:40 +0200

    mailbox.ts

---
+++ packages/expo/src/Notifications/Mailbox.ts
@@ -0,0 +1,43 @@
+import DeviceEventEmitter from 'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter';
+
+export class Mailbox {
+  onUserInteractionListeners: Map<string, OnUserInteractionListener>;
+  onForegroundNotificationListeners: Map<string, OnForegroundNotificationListener>;
+
+  constructor() {
+    this.onUserInteractionListeners = new Map();
+    this.onForegroundNotificationListener = new Map();
+    DeviceEventEmitter.addListener('Exponent.onUserInteraction', this._onUserInteractionListener);
+    DeviceEventEmitter.addListener('Exponent.onForegroundNotification', this._onForegroundNotification);
+  }
+
+  addOnUserInteractionListener(listenerName: string, listener: OnUserInteractionListener) {
+    this.onUserInteractionListeners.set(listenerName, listener);
+  }
+
+  addOnForegroundNotificationListener(listenerName: string, listener: OnForegroundNotificationListener) {
+    this.onForegroundNotificationListeners.set(listenerName, listener);
+  }
+
+  removeOnUserInteractionListener(listenerName: string) {
+    this.onUserInteractionListeners.delete(listenerName);
+  }
+
+  removeOnForegroundNotificationListener(listenerName: string) {
+    this.onForegroundNotificationListeners.delete(listenerName);
+  }
+
+  _onForegroundNotification(notification) {
+    const data = notification.data;
+    for (let listener of this.onUserInteractionListeners.values()) {
+      listener(data);
+    }
+  }
+
+  _onUserInteractionListener(userInteraction) {
+    const data = userInteraction.data;
+    for (let listener of this.onForegroundNotificationListeners.values()) {
+      listener(data);
+    }
+  }
+};
